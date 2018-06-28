d6813fc647b26beab9d141b15956a3c642f382ad
Kaylee <kayleevandevoorde@gmail.com>
2018-06-28 14:28:56 -0600

    Initial commit for Kaylee

---
+++ propagandaMachine/Propogate.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace propagandaMachine
+{
+    partial class Propagate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Propagate));
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.runPP = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.start.FlatAppearance.BorderSize = 5;
+            this.start.Font = new System.Drawing.Font("Modern No. 20", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(440, 166);
+            this.start.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(393, 89);
+            this.start.TabIndex = 0;
+            this.start.Text = "Propagate!";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.stop.Cursor = System.Windows.Forms.Cursors.Default;
+            this.stop.Enabled = false;
+            this.stop.Font = new System.Drawing.Font("Modern No. 20", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop.Location = new System.Drawing.Point(623, 280);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(393, 93);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "Stop!";
+            this.stop.UseVisualStyleBackColor = false;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // runPP
+            // 
+            this.runPP.Interval = 120000;
+            this.runPP.Tick += new System.EventHandler(this.runPP_Tick);
+            // 
+            // Propagate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1421, 519);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Font = new System.Drawing.Font("Modern No. 20", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Propagate";
+            this.Text = "TheMachine";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Timer runPP;
+    }
+}
+
---
+++ propagandaMachine/Propogate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Timers;
+
+
+namespace propagandaMachine
+{
+    public partial class Propagate : Form
+    {
+        Process openBrowser = new Process();
+        string powerPointProject = @"G:\MyDrive\FactionScoreboard4-4-18.pptx";
+        string powerPointOpener = @"C:\Program Files\Microsoft Office\Office16\POWERPNT.exe";        
+        string tinyPulse = @"https://app.tinypulse.com/engage?cheerfeed#sharing/cheer_feed?url=https%3A%2F%2Fapp.tinypulse.com%2Fapi%2Fcheers%3Factive_only%3Dtrue%26api_token%3Ddaf4e35c0eaf2489e11ab0%26organization_id%3D18876";
+        string mozilla = @"C:\Program Files (x86)\Mozilla Firefox\firefox.exe";
+
+        public Propagate()
+        {
+            InitializeComponent();
+
+            System.Timers.Timer powerPointRepeat = new System.Timers.Timer();
+            powerPointRepeat.Elapsed += new ElapsedEventHandler(runPP_Tick);
+            powerPointRepeat.Interval = 2*60*1000;
+        }
+
+        public void startBrowser(string file, string url)
+        {
+            openBrowser.StartInfo.FileName = file;
+            openBrowser.StartInfo.Arguments = string.Format(url);
+            openBrowser.Start();
+        }
+
+        public void killBrowser()
+        {
+            if (!openBrowser.HasExited)
+                openBrowser.CloseMainWindow();
+        }
+
+        public void startPowerPointSlideShow (string ppAppfile, string ppfile)
+        {
+            Process openPower = new Process();
+            openPower.StartInfo.FileName = ppAppfile;
+            openPower.StartInfo.Arguments = string.Format("{0} {1}", "/s", ppfile);
+            openPower.Start();
+            openPower.WaitForExit();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            stop.Enabled = true;
+            startBrowser(mozilla, tinyPulse);
+            start.Enabled = false;
+            runPP.Enabled = true; 
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            runPP.Enabled = false;
+            killBrowser();
+            start.Enabled = true;
+            stop.Enabled = false;
+        }
+
+        private void runPP_Tick(object sender, EventArgs e)
+        {
+            startPowerPointSlideShow(powerPointOpener, powerPointProject);
+        }
+    }
+}
